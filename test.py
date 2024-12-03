ff99ad0a42d8d3a81e769aa1508710b162e4cb00
dev2 <dev2@company.com>
2024-12-03 09:10:31 +0100

    add sum function

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 
-def Deduct(a,b):
+a = 10
+b = 5
+
+def Deduct(a:int,b:int):
     return a - b
 
-def Count(a,b):
+def Count(a:int,b:int):
     return a + b
+
+def Sum() -> int:
+    return Count(a,b) + Deduct(a,b)
 
 print(f"The result is: {Count(2,3)}")
 print(f"The relsutl deduct is: {Deduct(10,8)}")
-print("Hello world")
+print(f"Sum is: {Sum()}")

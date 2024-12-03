f9cf8d97b8dbe3d08423d4f21f26dc7d062f39e6
dev2 <dev2@company.com>
2024-12-03 09:38:57 +0100

    print delete

--- test.py
+++ test.py
@@ -11,6 +11,5 @@ def Count(a:int,b:int):
 def Sum() -> int:
     return Count(a,b) + Deduct(a,b)
 
-print(f"The result is: {Count(2,3)}")
 print(f"The relsutl deduct is: {Deduct(10,8)}")
 print(f"Sum is: {Sum()}")
